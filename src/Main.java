9e40115d5a0c580f31c03ee13d72973fe3e5dde2
614925 <senter.614925@swl.k12.oh.us>
2024-11-13 08:52:59 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = InputHelper.getInt(scan, "Enter your favorite Int: ");
+        double y = InputHelper.getDouble(scan, "Enter your favorite Double: ");
+
+        System.out.println("Your favorite int is: " + x + " and your favorite double is: " + y);
+    }
 }
